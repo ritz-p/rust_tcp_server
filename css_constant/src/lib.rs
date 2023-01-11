604a73c96ac025c0c88bc882413e838d122d15e4
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-01-12 00:39:18 +0900

    add,fix: design improved, type changed

--- css_constant/src/lib.rs
+++ css_constant/src/lib.rs
@@ -6,14 +6,15 @@ fn get_parts_css(tag: String,content: String) -> String{
 
 
 
-pub fn create_css(properties: HashMap<String,Vec<(String,String)>>) -> String{
+pub fn create_css(properties: HashMap<&str,Vec<(&str,&str)>>) -> String{
     let mut content = String::from("");
     for (tag,value) in properties.iter(){
+        let mut style = String::from("");
         for propertie in value{
-            let added_content = propertie.0.to_string() + &String::from(": ") + &propertie.1.to_string() + &String::from(";\n");
-            content += &added_content;
+            let added_content = propertie.0.to_string() + &String::from(": ") + &propertie.1 + &String::from(";\n");
+            style += &added_content;
         }
-        content = get_parts_css(tag.to_string(), content);
+        content += &get_parts_css(tag.to_string(), style);
     }
     content
 }
--- css_parts/src/monochrome_vertical_table_css/lib.rs
+++ css_parts/src/monochrome_vertical_table_css/lib.rs
@@ -2,9 +2,26 @@ use std::collections::HashMap;
 use css_constant::create_css;
 
 pub fn create_monochrome_vertical_table_css() -> String{
-    let mut table:Vec<(String,String)> = vec![];
-    table.push(("border".to_string(),"solid 1px".to_string()));
-    let mut css_hash:HashMap<String,Vec<(String,String)>> = HashMap::new();
-    css_hash.insert("table".to_string(),table);
+    let mut table:Vec<(&str,&str)> = vec![];
+    table.push(("width","100%"));
+    table.push(("border","solid 2px black"));
+    table.push(("border-collapse","collapse"));
+
+    let mut th: Vec<(&str,&str)> = vec![];
+    th.push(("color","#fff"));
+    th.push(("background-color","#222"));
+    th.push(("border","dashed 1px white"));
+
+    let mut td: Vec<(&str,&str)> = vec![];
+    td.push(("color","#222"));
+    td.push(("background-color","#fff"));
+    td.push(("border","dashed 1px black"));
+    td.push(("text-align","center"));
+    
+
+    let mut css_hash:HashMap<&str,Vec<(&str,&str)>> = HashMap::new();
+    css_hash.insert("table",table);
+    css_hash.insert("th",th);
+    css_hash.insert("td",td);
     create_css(css_hash)
 }
