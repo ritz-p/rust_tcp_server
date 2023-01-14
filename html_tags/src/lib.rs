df4828cfc7fcac04074225153b365585d9f33fa2
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-01-15 00:23:39 +0900

    fix: move tag

--- html_tags/src/lib.rs
+++ html_tags/src/lib.rs
@@ -1,6 +1,48 @@
 pub mod tag;
 pub mod html_type;
 
+pub struct Tag<'a>{
+    variable: TagName<'a>,
+}
+
+pub trait SingleTag{
+    fn get_tag(&self) -> String;
+    fn bind(&self,content:String,use_end_slash:bool) -> String;
+}
+
+pub trait PairTag{
+    fn get_start_tag(&self) -> String;
+    fn get_end_tag(&self) -> String;
+    fn bind(&self,content: String) -> String;
+}
+
+impl SingleTag for Tag<'_>{
+    fn get_tag(&self) -> String {
+        self.variable.get_tag_name().to_owned()
+    }
+    fn bind(&self,content: String,use_end_slash: bool) -> String{
+        let mut ret = String::from("<") + &self.variable.get_tag_name().to_owned() + &content;
+        if use_end_slash{
+            ret = ret + &String::from("/>")
+        }else{
+            ret = ret + &String::from(">")
+        }
+        ret
+    }
+}
+
+impl PairTag for Tag<'_>{
+    fn get_start_tag(&self)->String{
+        String::from("<") + &self.variable.get_tag_name().to_owned() + &String::from(">")
+    }
+    fn get_end_tag(&self)->String{
+        String::from("</")+ &self.variable.get_tag_name().to_owned() + &String::from(">")
+    }
+    fn bind(&self,content:String)->String{
+        self.get_start_tag() + &content + &self.get_end_tag()
+    }
+}
+
 impl TagName<'_>{
     fn get_tag_name(&self) -> &str{
         match self {
